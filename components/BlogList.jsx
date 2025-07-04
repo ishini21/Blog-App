0ed0f1b3cf2028ee443c218b67e1a1bb9be9de4b
ishini shavindhya <ishinishavindhya@gmail.com>
2025-07-04 15:08:05 +0530

    blogs filter function

--- components/BlogList.jsx
+++ components/BlogList.jsx
@@ -1,22 +1,32 @@
 import { blog_data } from "@/assets/assets";
-import React from "react";
+import React, { useState } from "react";
 import BlogItem from "./BlogItem";
 
 function BlogList() {
+  const [menu, setMenu] = useState("All");
+
   return (
     <div>
       <div className="flex justify-center gap-6 my-10">
-        <button className="bg-blue-900 text-white py-1 px-4 rounded-sm">
+        <button onClick={()=>setMenu("All")} className={menu==="All"?"bg-blue-900 text-white py-1 px-4 rounded-sm":""}>
           All
         </button>
-        <button className="">Technology</button>
-        <button className="">Startup</button>
-        <button className="">Lifestyle</button>
+        <button  onClick={()=>setMenu("Technology")}className={menu==="Technology"?"bg-blue-900 text-white py-1 px-4 rounded-sm":""}>Technology</button>
+        <button  onClick={()=>setMenu("Startup")}className={menu==="Startup"?"bg-blue-900 text-white py-1 px-4 rounded-sm":""}>Startup</button>
+        <button  onClick={()=>setMenu("Lifestyle")}className={menu==="Lifestyle"?"bg-blue-900 text-white py-1 px-4 rounded-sm":""}>Lifestyle</button>
       </div>
       <div className="flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24">
-        {blog_data.map((item,index)=>{
-        return <BlogItem key={index} image={item.image} title={item.title} description={item.description} category={item.category}
-        />})}
+        {blog_data.filter((item)=>menu==='All'?true:item.category===menu).map((item, index) => {
+          return (
+            <BlogItem
+              key={index}
+              image={item.image}
+              title={item.title}
+              description={item.description}
+              category={item.category}
+            />
+          );
+        })}
       </div>
     </div>
   );
