edf95e62e84c4c28daa21e81e231bcda862d0765
ishini shavindhya <ishinishavindhya@gmail.com>
2025-07-04 12:42:18 +0530

    Initial Commit

---
+++ components/Header.jsx
@@ -0,0 +1,14 @@
+import { assets } from '@/Assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='py-5 px-5 md:px-12 lg:px-28'>
+        
+      
+    </div>
+  )
+}
+
+export default Header
