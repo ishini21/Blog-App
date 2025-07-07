0f460f819c02283e456a85e19b83743821c8bd2f
ishini shavindhya <ishinishavindhya@gmail.com>
2025-07-07 12:20:40 +0530

    created blog adding api

--- app/page.js
+++ app/page.js
@@ -3,7 +3,8 @@ import BlogItem from "@/components/BlogItem";
 import BlogList from "@/components/BlogList";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-
+//1vsgkmI6ThlG6GTq
+//mongodb+srv://ishinishavindhya:1vsgkmI6ThlG6GTq@cluster0.nr2kkmu.mongodb.net/
 
 export default function Home() {
   return (
