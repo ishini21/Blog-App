94823320eeb37017ed8f41c92de5f5e6aca3313c
ishini shavindhya <ishinishavindhya@gmail.com>
2025-07-07 15:10:59 +0530

    created addProduct.jsx

--- app/admin/addProduct/page.jsx
+++ app/admin/addProduct/page.jsx
@@ -1,13 +1,111 @@
-'use client'
+"use client";
 
-import React from 'react'
+import { assets } from "@/assets/assets";
+import React, { useState } from "react";
+import Image from "next/image";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 function page() {
+  const [image, setImage] = useState(false);
+  const [data, setData] = useState({
+    title: "",
+    description: "",
+    category: "Startup",
+    author: "Alex Bennett",
+    authorImage: "/author_img.png",
+  });
+
+  const onChangeHandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setData((data) => ({ ...data, [name]: value }));
+    console.log(data);
+  };
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("title", data.title);
+    formData.append("description", data.description);
+    formData.append("category", data.category);
+    formData.append("author", data.author);
+    formData.append("authorImg", data.authorImage);
+    formData.append("image", image);
+    const response = await axios.post("/api/blog", formData);
+    if (response.data.success) {
+      toast.success(response.data.msg);
+      setImage(false);
+      setData({
+        title: "",
+        description: "",
+        category: "Startup",
+        author: "Alex Bennett",
+        authorImage: "/author_img.png",
+      });
+    } else {
+      toast.error("Error");
+    }
+  };
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <form onSubmit={onSubmitHandler} className="pt-5 px-5 sm:pt-12 sm:pl-16">
+        <p className="text-xl">Upload thummbmail</p>
+        <label htmlFor="image">
+          <Image
+            src={!image ? assets.upload_area : URL.createObjectURL(image)}
+            width={140}
+            height={70}
+            alt=""
+            className="mt-4"
+          />
+        </label>
+        <input
+          onChange={(e) => setImage(e.target.files[0])}
+          type="file"
+          id="image"
+          hidden
+          required
+        />
+        <p className="text-xl mt-4">Blog title</p>
+        <input
+          name="title"
+          onChange={onChangeHandler}
+          value={data.title}
+          type="text"
+          placeholder="Type here"
+          required
+          className="w-full sm:w-[500px] mt-4 px-4 py-3 border"
+        />
+        <p className="text-xl mt-4">Blog description</p>
+        <textarea
+          name="description"
+          onChange={onChangeHandler}
+          value={data.description}
+          type="text"
+          placeholder="Write content here"
+          required
+          rows={6}
+          className="w-full sm:w-[500px] mt-4 px-4 py-3 border"
+        />
+        <p className="text-xl mt-4">Blog category</p>
+        <select
+          name="category"
+          onChange={onChangeHandler}
+          value={data.category}
+          className="w-40 mt-4 px-4 py-3 border text-gray-500 "
+        >
+          <option value="Startup">Startup</option>
+          <option value="Technology">Technology</option>
+          <option value="Lifestyle">Lifestyle</option>
+        </select>
+        <br />
+        <button type="submit" className="mt-8 w-40 h-12 bg-black text-white">
+          ADD
+        </button>
+      </form>
+    </>
+  );
 }
 
-export default page
+export default page;
--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -1,6 +1,7 @@
 import { assets } from "@/assets/assets";
 import Sidebar from "@/components/AdminComponents/Sidebar";
 import Image from "next/image";
+import { ToastContainer} from 'react-toastify';
 
 export default function Layout({
     children
@@ -8,15 +9,17 @@ export default function Layout({
     return(
         <>
         <div className="flex">
+            <ToastContainer theme="dark"/>
             <Sidebar/>
             <div className="flex flex-col w-full ">
                 <div className="flex items-center justify-between w-full py-3 max-h-[60px] px-12 border-b border-black ">
                     <h3>Admin Panel</h3>
                     <Image src={assets.profile_icon} alt='' width={40} />
                 </div>
+                 {children}
             </div>
         </div>
-        {children}
+       
         </>
     )
 }
