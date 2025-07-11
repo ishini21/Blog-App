0ff40dcd076f6c4f5376dfd04ef3579a8b560e39
ishini shavindhya <ishinishavindhya@gmail.com>
2025-07-11 16:17:45 +0530

    Updated login,signup UI

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -24,21 +24,23 @@ export default function Login() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center ">
-    <div className="max-w-md mx-auto p-6 shadow-sm rounded-lg">
-      <h2 className="text-3xl text-blue-900 font-bold mb-4 text-center">Login</h2>
-      <input className="border border-blue-100 p-2 w-full mb-2" placeholder="Email"
+    <>
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-green-100 to-blue-300 ">
+    <div className="bg-transparent max-w-md mx-auto p-6 shadow-sm rounded-lg ">
+      <h2 className="text-3xl text-[#0891b2] font-bold mb-4 text-center">Login</h2>
+      <input className="bg-white border border-blue-100 focus:border-none focus:outline-none p-2 w-full mb-2" placeholder="Email" required
         onChange={(e) => setForm({ ...form, email: e.target.value })} />
-      <input className="border border-blue-100 p-2 w-full mb-2" placeholder="Password" type="password"
+      <input className="bg-white border border-blue-100 focus:border-none focus:outline-none p-2 w-full mb-2" placeholder="Password" type="password" required
         onChange={(e) => setForm({ ...form, password: e.target.value })} />
-      <button onClick={handleLogin} className="bg-blue-900 text-white p-2 w-full mt-4 text-lg">Login</button>
+      <button onClick={handleLogin} className="bg-[#0891b2] text-white p-2 w-full mt-4 text-lg">Login</button>
        <div className="">
             <p className="text-black text-center text-xs mt-4">
             Don't have an account? {""}
-            <Link href='/signup'><span className='text-blue-900 cursor-pointer underline'>SignUp</span>
+            <Link href='/signup'><span className='text-blue-950 cursor-pointer underline'>SignUp</span>
             </Link></p>
         </div>
     </div>
     </div>
+    </>
   );
 }
--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -24,29 +24,31 @@ export default function Signup() {
   };
 
   return (
-  <div className="min-h-screen flex items-center justify-center ">
-    <div className="max-w-md mx-auto p-6 bg-white text-center shadow-sm rounded-lg">
-      <h2 className="text-3xl font-bold mb-4 text-blue-900">Sign Up</h2>
-      <input className="border border-blue-100 p-2 w-full mb-2" placeholder="Name"
+    <>
+  <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-green-100 to-blue-300 ">
+    <div className="max-w-md mx-auto p-6 bg-transparent text-center shadow-sm rounded-lg">
+      <h2 className="text-3xl font-bold mb-4 text-[#0891b2]">Sign Up</h2>
+      <input className="bg-white border border-blue-100 focus:border-none focus:outline-none p-2 w-full mb-2" placeholder="Name" required
         onChange={(e) => setForm({ ...form, name: e.target.value })} />
-      <input className="border border-blue-100 p-2 w-full mb-2" placeholder="Email" type="email"
+      <input className="bg-white border border-blue-100 focus:border-none focus:outline-none p-2 w-full mb-2" placeholder="Email" type="email" required
         onChange={(e) => setForm({ ...form, email: e.target.value })} />
-      <input className="border border-blue-100 p-2 w-full mb-2" placeholder="Password" type="password"
+      <input className="bg-white border border-blue-100 focus:border-none focus:outline-none p-2 w-full mb-2" placeholder="Password" type="password" required
         onChange={(e) => setForm({ ...form, password: e.target.value })} />
-      <select className="border border-blue-100 p-2 w-full mb-2"
+      <select className="bg-white border border-blue-100 focus:border-none focus:outline-none p-2 w-full mb-2" required
         onChange={(e) => setForm({ ...form, role: e.target.value })}>
         
         <option value="user">User</option>
         <option value="admin">Admin</option>
       </select>
-      <button onClick={handleSignup} className="bg-blue-900 text-white p-2 w-full mt-4 text-lg">Signup</button>
+      <button onClick={handleSignup} className="bg-[#0891b2] text-white p-2 w-full mt-4 text-lg">Signup</button>
        <div className="">
             <p className="text-black text-center text-xs mt-4 ">
             Already have an account? {""}
-            <Link href='/login'><span className='text-blue-900 cursor-pointer underline'>Login</span>
+            <Link href='/login'><span className='text-blue-950 cursor-pointer underline'>Login</span>
             </Link></p>
         </div>
     </div>
     </div>
+    </>
   );
 }
