d1640b6c2bef4ced09f341fb12ec36774bc6dc86
ishini shavindhya <ishinishavindhya@gmail.com>
2025-07-14 09:54:15 +0530

    updated Footer.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -55,11 +55,11 @@ function Header() {
         </div>
       </div>
 
-      <div className="text-center my-8 bg-white p-10  rounded-lg border border-none">
-        <h1 className="text-3xl sm:text-5xl font-medium text-[#0891b2]">
+      <div className="text-center my-8 bg-[#0891b2] p-10  rounded-lg border border-none">
+        <h1 className="text-3xl sm:text-5xl font-medium text-white">
           Latest Blogs
         </h1>
-        <p className="mt-10 max-w-[740px] m-auto text-xs sm:text-base">
+        <p className="mt-10 max-w-[740px] m-auto text-xs sm:text-base text-white">
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
           Exercitationem dolor unde nobis fugit dignissimos blanditiis veritatis
           commodi! Cum voluptatum quod esse error similique aut. Doloribus optio
@@ -67,19 +67,19 @@ function Header() {
         </p>
         <form
           onSubmit={onSubmitHandler}
-          className="flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#0891b2]"
+          className="flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]"
         >
           <input
             onChange={(e) => setEmail(e.target.value)}
             value={email}
             type="email"
             placeholder="Enter your email"
-            className="pl-4 outline-none"
+            className="pl-4 outline-none text-white"
           />
 
           <button
             type="submit"
-            className="border-1 border-black py-4 px-4 sm:px-8 active:bg-gray-600 active:text-white"
+            className="border-1 border-black py-4 px-4 sm:px-8 active:bg-gray-600 active:text-white text-white"
           >
             Subscribe
           </button>
