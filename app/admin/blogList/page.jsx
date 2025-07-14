8b671a01612a1b18154385f7a2c0abcbc02db00d
ishini shavindhya <ishinishavindhya@gmail.com>
2025-07-14 10:16:38 +0530

    Updated blogList page.jsx

--- app/admin/blogList/page.jsx
+++ app/admin/blogList/page.jsx
@@ -30,24 +30,24 @@ function page() {
 
   return (
     <div className="flex-1 pt-5  px-5 sm:pt-12 sm:pl-16">
-      <h1>All blogs</h1>
+      <h1 className="text-xl text-[#0891b2] font-bold">All blogs</h1>
       <div className="relative h-[80vh] max-w-[850px] overflow-x-auto mt-4 border border-gray-400 scrollbar-hide">
         <table className="w-full text-sm text-gray-500">
           <thead className="text-sm text-gray-700 text-left uppercase bg-gray-50">
             <tr>
-              <th scope="" className="hidden sm:block px-6 py-3">
+              <th scope="" className="hidden sm:block px-6 py-3 text-[#0891b2]">
                 Author name
               </th>
-              <th scope="" className="px-6 py-3">
+              <th scope="" className="px-6 py-3 text-[#0891b2]">
                 Blog title
               </th>
               {/* <th scope="" className="px-6 py-3">
                 Blog name
               </th> */}
-              <th scope="" className="px-6 py-3">
+              <th scope="" className="px-6 py-3 text-[#0891b2]">
                 Date
               </th>
-              <th scope="" className="px-6 py-3">
+              <th scope="" className="px-6 py-3  text-[#0891b2]">
                 Action
               </th>
             </tr>
--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -13,7 +13,7 @@ export default function Layout({
             <Sidebar/>
             <div className="flex flex-col w-full bg-gradient-to-br from-green-100 to-blue-300 ">
                 <div className="flex items-center justify-between w-full py-3 max-h-[60px] px-12 border-b border-black bg-gradient-to-br from-green-100 to-blue-300 ">
-                    <h3>Admin Panel</h3>
+                    <h3 className="text-xl text-black font-bold">Admin Panel</h3>
                     <Image src={assets.profile_icon} alt='' width={40} />
                 </div>
                  {children}
