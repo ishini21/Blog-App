f7f0ac44c407d7499dc60e60783c61ede9a46b63
ishini shavindhya <ishinishavindhya@gmail.com>
2025-07-15 10:19:17 +0530

    added card view to blogs

--- components/BlogItem.jsx
+++ components/BlogItem.jsx
@@ -5,14 +5,14 @@ import React from "react";
 
 function BlogItem({ title, description, category, image, id }) {
   return (
-    <div className="max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#0891b2]">
+    <div className="max-w-[330px] sm:max-w-[300px] bg-white border border-gray-300 rounded-lg shadow-md transform transition-transform hover:shadow-xl hover:-translate-y-1 hover:scale-[1.03] duration-400 ">
       <Link href={`/blogs/${id}`}>
         <Image
           src={image}
           alt={title || "Blog post image"}
           width={400}
           height={400}
-          className="border border-black"
+          className="border rounded-lg p-2"
         />
       </Link>
       <p className="ml-5 mt-5 px-1 inline-block bg-[#0891b2] text-white text-sm">
@@ -22,10 +22,13 @@ function BlogItem({ title, description, category, image, id }) {
         <h5 className="mb-2 text-lg font-medium tracking-tight text-gray-900">
           {title}
         </h5>
-        <p 
+        <p
           className="mb-3 text-sm tracking-tight text-gray-700"
           dangerouslySetInnerHTML={{
-            __html: description?.slice(0, 120) + (description?.length > 120 ? "..." : "") || "No description available"
+            __html:
+              description?.slice(0, 120) +
+                (description?.length > 120 ? "..." : "") ||
+              "No description available",
           }}
         />
         <Link
@@ -33,11 +36,11 @@ function BlogItem({ title, description, category, image, id }) {
           className="inline-flex items-center py-2 font-semibold text-center"
         >
           Read more{" "}
-          <Image 
-            src={assets.arrow} 
-            width={12} 
-            alt="Read more arrow" 
-            className="ml-2" 
+          <Image
+            src={assets.arrow}
+            width={12}
+            alt="Read more arrow"
+            className="ml-2"
           />
         </Link>
       </div>
--- components/BlogList.jsx
+++ components/BlogList.jsx
@@ -5,41 +5,76 @@ import axios from "axios";
 
 function BlogList() {
   const [menu, setMenu] = useState("All");
-  const [blogs,setBlogs] = useState([]);
+  const [blogs, setBlogs] = useState([]);
 
   const fetchBlogs = async () => {
-    const response = await axios.get('/api/blog');
+    const response = await axios.get("/api/blog");
     setBlogs(response.data.blogs);
-    console.log(response.data.blogs)
-  }
+    console.log(response.data.blogs);
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchBlogs();
-  },[])
+  }, []);
 
   return (
     <div className="">
       <div className="flex justify-center gap-6 my-10">
-        <button onClick={()=>setMenu("All")} className={menu==="All"?"bg-[#0891b2] text-white py-1 px-4 rounded-sm":""}>
+        <button
+          onClick={() => setMenu("All")}
+          className={
+            menu === "All" ? "bg-[#0891b2] text-white py-1 px-4 rounded-sm" : ""
+          }
+        >
           All
         </button>
-        <button  onClick={()=>setMenu("Technology")}className={menu==="Technology"?"bg-[#0891b2] text-white py-1 px-4 rounded-sm":""}>Technology</button>
-        <button  onClick={()=>setMenu("Startup")}className={menu==="Startup"?"bg-[#0891b2] text-white py-1 px-4 rounded-sm":""}>Startup</button>
-        <button  onClick={()=>setMenu("Lifestyle")}className={menu==="Lifestyle"?"bg-[#0891b2] text-white py-1 px-4 rounded-sm":""}>Lifestyle</button>
+        <button
+          onClick={() => setMenu("Technology")}
+          className={
+            menu === "Technology"
+              ? "bg-[#0891b2] text-white py-1 px-4 rounded-sm"
+              : ""
+          }
+        >
+          Technology
+        </button>
+        <button
+          onClick={() => setMenu("Startup")}
+          className={
+            menu === "Startup"
+              ? "bg-[#0891b2] text-white py-1 px-4 rounded-sm"
+              : ""
+          }
+        >
+          Startup
+        </button>
+        <button
+          onClick={() => setMenu("Lifestyle")}
+          className={
+            menu === "Lifestyle"
+              ? "bg-[#0891b2] text-white py-1 px-4 rounded-sm"
+              : ""
+          }
+        >
+          Lifestyle
+        </button>
       </div>
-      <div className="flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24">
-        {blogs.filter((item)=>menu==='All'?true:item.category===menu).map((item, index) => {
-          return (
-            <BlogItem
-              key={index}
-              id={item._id}
-              image={item.image}
-              title={item.title}
-              description={item.description}
-              category={item.category}
-            />
-          );
-        })}
+      {/* GRID CONTAINER */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 px-5 mb-16 xl:mx-24">
+        {blogs
+          .filter((item) => (menu === "All" ? true : item.category === menu))
+          .map((item, index) => {
+            return (
+              <BlogItem
+                key={index}
+                id={item._id}
+                image={item.image}
+                title={item.title}
+                description={item.description}
+                category={item.category}
+              />
+            );
+          })}
       </div>
     </div>
   );
